6dc753abad493574c0abf6e39244e8f9b90c7333
AlecMorganNSC <Alec.Morgan@seattlecolleges.edu>
2025-07-18 21:50:32 -0700

    Added more print statements

--- Lab0/src/helloWorld/HelloWorld.java
+++ Lab0/src/helloWorld/HelloWorld.java
@@ -3,5 +3,8 @@ package helloWorld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.out.println("Course:        CSC 142");
+		System.out.println("Professor:     Solmaz Monir");
+		System.out.println("Learned:       EGit is nifty!");
 	}
 }
